ee3dc8ebab25995984a562e4e041ea9e55ab05f8
Jim <jimba.ed@outlook.com>
2022-08-30 14:12:08 -0400

    commit tree 70a6ab96abf04c2bca5c30365ac1a1653399d3ee

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity){
+    switch(activity){
+        default:
+            return "This Saturday, I want to roller-skate!" 
+        case 'bathe my dog':
+            return "This Saturday, I want to bathe my dog!"
+    } 
+}
+function mondayWork(activity){
+    switch(activity){
+            default:
+                return "This Monday, I will go to the office."
+            case 'work from home':
+                return "This Monday, I will work from home."
+    }
+}
+function wrapAdjective(adj){
+    return function(emphText){
+        return `You are ${adj}${emphText}${adj}!`
+    }
+}
